6638fb94e5e9adeba04b9dd45396d3ae732d894d
steveandjeff999 <150098465+steveandjeff999@users.noreply.github.com>
2025-10-09 22:34:00 -0500

    fixes

---
+++ app/instance/plugins/51bd0a6a02b0d9da4804051aa61332622509a9e962dd27bda11b647dc3b8caf3/plugin_template.py
@@ -0,0 +1,8 @@
+# Example plugin template for ObsidianScout
+# This plugin prints a message when executed.
+
+def main():
+    print('Hello from plugin template!')
+
+if __name__ == '__main__':
+    main()
